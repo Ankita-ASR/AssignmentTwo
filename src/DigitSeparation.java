239a6b8f71faeec1277b8939ce39f26f42c98500
Ankita <ankita7saha@gmail.com>
2024-03-24 00:40:55 +0600

    E4.15

--- src/DigitSeparation.java
+++ src/DigitSeparation.java
@@ -2,7 +2,6 @@
 import java.util.Scanner;
 public class DigitSeparation {
     public static void main(String[] args) {
-        int i;
         Scanner scanner = new Scanner(System.in);
         System.out.print("Please enter an integer: ");
         int number = scanner.nextInt();
